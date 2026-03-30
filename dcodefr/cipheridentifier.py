66d31a939e3a606249e427789c23319d9806c7ec
garfield <garfield@localhost.localdomain>
2026-03-29 20:42:44 -0700

    fixed chrome selenium

--- dcodefr/cipheridentifier.py
+++ dcodefr/cipheridentifier.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import 
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 def loadwebsite(site, ciphertext):
