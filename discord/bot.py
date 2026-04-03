5b219fe17e5b6d432af7cdf0091bdbed1d0d91a1
garfield <garfield@localhost.localdomain>
2026-04-02 18:34:50 -0700

    web server

--- discord/bot.py
+++ discord/bot.py
@@ -32,17 +32,40 @@ async def on_message(message):
         County = message.content.split(" ")[2:len(message.content.split(" "))]
         County = " ".join(County)
         await message.channel.send(getincomekeys(numofhousehold, County))
+localserver = False
+if localserver:
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
 
+    #application ID: 1487685826984022087
+    #Public key: b00006a9a6d636edaedaee0ba3b2906be6f4abf8277a95ff2110ac20089c4789
+    #Token MTQ4NzY4NTgyNjk4NDAyMjA4Nw.GvZofe.nl4r9YbH52iQkIyPKtZrJ5e1YWo7oFMgRrG2MM
+    discordToken="MTQ4NzY4NTgyNjk4NDAyMjA4Nw.GvZofe.nl4r9YbH52iQkIyPKtZrJ5e1YWo7oFMgRrG2MM"
+    client.run(discordToken)
+else:
+    async def handle(request):
+        return web.Response(text="Bot alive")
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    async def start_webserver():
+        app = web.Application()
+        app.router.add_get("/", handle)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        port = int(os.environ.get("PORT", 8080))
+        site = web.TCPSite(runner, "0.0.0.0", port)
+        await site.start()
 
-#application ID: 1487685826984022087
-#Public key: b00006a9a6d636edaedaee0ba3b2906be6f4abf8277a95ff2110ac20089c4789
-#Token MTQ4NzY4NTgyNjk4NDAyMjA4Nw.GvZofe.nl4r9YbH52iQkIyPKtZrJ5e1YWo7oFMgRrG2MM
-discordToken="MTQ4NzY4NTgyNjk4NDAyMjA4Nw.GvZofe.nl4r9YbH52iQkIyPKtZrJ5e1YWo7oFMgRrG2MM"
-client.run(discordToken)
+    @client.event
+    async def on_ready():
+        print(f"Logged in as {client.user}")
+
+    async def main():
+        await start_webserver()
+        discordToken="MTQ4NzY4NTgyNjk4NDAyMjA4Nw.GvZofe.nl4r9YbH52iQkIyPKtZrJ5e1YWo7oFMgRrG2MM"
+        await client.start(discordToken)
+
+    asyncio.run(main())
